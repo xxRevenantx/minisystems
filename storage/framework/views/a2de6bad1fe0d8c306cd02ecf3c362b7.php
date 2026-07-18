3664b8c2a9fae1c3ab91a04576c5085fafbb86d9
xxRevenantx <beto_war_157@hotmail.com>
2026-07-18 00:12:02 -0600

    Cambios

--- storage/framework/views/a2de6bad1fe0d8c306cd02ecf3c362b7.php
+++
@@ -1,4 +0,0 @@
-
-
-<div <?php echo e($attributes->class('flex-1')); ?> data-flux-spacer></div>
-<?php /**PATH C:\laragon\www\minisystems\vendor\livewire\flux\src/../stubs/resources/views/flux/spacer.blade.php ENDPATH**/ ?>
--- storage/framework/views/ceaf07d976dcc28b0fec43d8d0efb217.php
+++
@@ -1,7 +0,0 @@
-
-
-<ui-menu-radio-group <?php echo e($attributes); ?> data-flux-menu-radio-group>
-    <?php echo e($slot); ?>
-
-</ui-menu-radio-group>
-<?php /**PATH C:\laragon\www\minisystems\vendor\livewire\flux\src/../stubs/resources/views/flux/menu/radio/group.blade.php ENDPATH**/ ?>
